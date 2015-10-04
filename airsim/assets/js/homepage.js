687a64b42221fa2ebe64dd0585b530d727780ef1
ajakupson <ajakupson@gmail.com>
2015-10-04 23:11:16 +0300

    Homepage mockups + start of friends page

--- airsim/assets/js/homepage.js
+++ airsim/assets/js/homepage.js
@@ -13,4 +13,35 @@ $(document).ready(function() {
 		}
 	});
 
+	setTimeout(function() {
+		$('#user_location_map').jHERE({
+	        enable: [],
+	        zoom: 10,
+	        center: { latitude: 59.437, longitude: 24.7536 },
+	        type: 'map',
+	        // API credentials from Nokia developer website
+	        appId: 'URXmsqAlIxjr41aSiojS', // appId from the Nokia developer website,
+	        authToken: '7w1lsMEuL9ToVILvmS8vLA'
+	    });
+	}, 1000);
+
+	setUserInfoAccordeonExpandCollapseButtonsHandlers();
+
+
+    function setUserInfoAccordeonExpandCollapseButtonsHandlers() {
+
+        $('body').on('click', '.accordeon h1' , function(e){ 
+            var $this = $(this);
+            var $accordeonPanel = $this.next();
+            var $collapseExpandBtn = $this.find('.fa');
+            if($collapseExpandBtn.hasClass('fa-arrow-down')) {
+                $collapseExpandBtn.removeClass('fa-arrow-down').addClass('fa-arrow-up');
+                $accordeonPanel.slideDown(500);
+            } else {
+                $collapseExpandBtn.removeClass('fa-arrow-up').addClass('fa-arrow-down');
+                $accordeonPanel.slideUp(500);
+            }
+        });
+    }
+
 });
